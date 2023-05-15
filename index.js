1eed54f0bd99d62efdd9956b41dce296d5142f06
SciMathist <74125575+SciMathist@users.noreply.github.com>
2023-05-15 17:32:11 +0000

    Updated.

--- index.js
+++ index.js
@@ -1,73 +1,195 @@
 const { Telegraf } = require('telegraf');
+const FormData = require('form-data');
 const axios = require('axios');
+const keepAlive = require('./keepAlive.js');
+const gtts = require('node-gtts')('en');
+const path = require('path'); //built-in
+const fs = require('fs'); //built-in
 
-// Set up the RapidAPI OCR API
-const rapidapiKey = 'ff8953e2b2mshfbd85d43df50f19p1b0e1bjsn3a980e29ae31';
-const rapidapiUrl = 'https://ocr-nanonets.p.rapidapi.com/';
+const delayTime = 500;
 
 // Create a new Telegraf bot instance
 const TELEGRAM_BOT_TOKEN = '5870366018:AAGGCvmKaJ5CD4qC0eQ0psVZpgmD3FmdMyc'
-const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
-
-// Register a photo message handler
-bot.on('photo', async (ctx) => {
-  try {
-    // Get the photo file ID from the message
-    const fileId = ctx.message.photo[ctx.message.photo.length - 1].file_id;
-    // Download the photo using the Telegram Bot API
-    const photo = await ctx.telegram.getFile(fileId);
-    // Send a message to let the user know the OCR is processing
-    await ctx.reply('Recognizing text...');
-    // Send the photo to the RapidAPI OCR API for processing
-    const headers = {
-      'X-RapidAPI-Host': 'ocr-nanonets.p.rapidapi.com',
-      'X-RapidAPI-Key':rapidapiKey,
-       'content-type': 'application/x-www-form-urlencoded'
-    };
-    const formData = {
-      image: photo.fileLink,
-      language: 'eng',
-      output: 'text'
-    };
-    const response = await axios.post(rapidapiUrl, formData, { headers: headers });
-    const result = response.data;
-    const text = result.data.text;
-    ctx.reply(text);
-  } catch (err) {
-    console.error(err);
-    ctx.reply('An error occurred while processing the image.');
-  }
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN, {
+  session: true,
 });
 
-// Customization....
+//API endpoint
+const API_ENDPOINT = 'https://ocr-nanonets.p.rapidapi.com/';
+
+// start command handler
 bot.start((ctx) => {
-  // Create a new session for the user
-  ctx.session = {};
+  // get the user's first name
+  const firstName = ctx.from.first_name;
 
-  // Send a welcome message
-  ctx.reply('Welcome to the OCR bot! Please send me a photo to recognize text from.');
+  // greet the user with a message
+  ctx.reply(`Hello, ${firstName}! I'm Zipso, an OCR-bot.🍀☘️🍁`);
+  setTimeout(() => {
+    ctx.reply('Send me a Photo or an Url.....🦖🦕🦚🐸🐲');
+  }, delayTime);
+
 });
 
+// listen for the /new command
 bot.command('new', (ctx) => {
-  // Create a new session for the user
+
+  // create a new session for the user
   ctx.session = {};
 
-  // Send a welcome message
-  ctx.reply('Welcome to the OCR bot! Please send me a photo to recognize text from.');
+  // send a message to confirm the new session
+
+  ctx.reply('A new session has been created.');
+  setTimeout(() => {
+    ctx.reply('Send me a Photo or an Url.....🦖🦕🦚🐸🐲');
+  }, delayTime);
+
 });
 
-//checks if the incoming message from the user contains a photo by checking the photo field in the message object.
-bot.on('message', (ctx) => {
-  if (ctx.message.photo) {
-    // Handle the photo
-  } else {
-    // Ask the user to send a photo
-    ctx.reply('Please send me a photo!');
+
+// listen for new users joining the chat
+bot.on('new_chat_members', (ctx) => {
+  // get the new user's first name
+  const firstName = ctx.message.new_chat_member.first_name;
+
+  // send a welcome message to the new user
+  ctx.reply(`Welcome, ${firstName}! I'm Zipso, an OCR-bot.🍀☘️🍁`);
+});
+
+
+
+//handle status command
+bot.command('status', (ctx) => {
+  ctx.reply('Click on the link to check service uptime status👉 https://scimathistlab.betteruptime.com/');
+});
+
+
+// Register the /speech command handler
+bot.command('speech', (ctx) => {
+  // Extract the text parameter from the command arguments
+  const text = ctx.message.text.substring(8);
+  if (!text) {
+    return ctx.reply('Please provide some text to speak.');
+  }
+  
+  // Generate speech from the text using node-gtts
+  const filePath = path.join(__dirname, 'speech.mp3');
+  gtts.save(filePath, text, function(err) {
+    if (err) {
+      console.log('Error:', err);
+      ctx.reply('Something went wrong! 🥲😥🙄');
+    } else {
+      console.log('Speech saved successfully');
+      // Reply with the audio file to the user
+      ctx.replyWithAudio({
+        source: filePath
+      }).then(() => {
+        // Delete the file after sending it
+        fs.unlinkSync(filePath);
+        console.log('File deleted successfully');
+      }).catch((err) => {
+        console.log('Error:', err);
+      });
+    }
+  });
+});
+
+  
+// Handle incoming messages  
+
+bot.on('message', async (ctx) => {
+  
+  // Check if the message contains a photo
+if (ctx.message.photo) {  
+    const photo = ctx.message.photo[2];
+    const fileId = photo.file_id;
+   const file = await ctx.telegram.getFile(fileId);
+   await ctx.reply('Thank you! The image will be processed now.🦩🦜🦚');
+if (!file.file_path) {
+  // try again after a short delay
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  const file = await ctx.telegram.getFile(fileId);
+  await ctx.reply('Thank you! The image will be processed now.🦩🦜🦚');
+  
+  if (!file.file_path) {
+    // file path is still undefined, handle the error
+    console.error('File path is undefined');
+    return;
+  }
+}
+    const fileUrl = `https://api.telegram.org/file/bot${TELEGRAM_BOT_TOKEN}/${file.file_path}`;
+    console.log('img-file received:', fileId);
+    console.log(fileUrl);
+
+    // Create a FormData object and append the photo to it
+    const data = new FormData();
+    data.append('urls', fileUrl);
+
+    // Set up options for the API request
+    const options = {
+      method: 'POST',
+      url: API_ENDPOINT,
+      headers: {
+        'X-RapidAPI-Key': 'ff8953e2b2mshfbd85d43df50f19p1b0e1bjsn3a980e29ae31',
+        'X-RapidAPI-Host': 'ocr-nanonets.p.rapidapi.com',
+        ...data.getHeaders(),
+      },
+      data: data
+    };
+
+    try {
+      // Make the API request and log the response
+      const response = await axios.request(options);
+      const rawText = response.data.results[0].page_data[0].raw_text;
+      await ctx.reply(rawText);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+      ctx.reply('Something went wrong!')
+    }
+  
+} else {
+  
+  if (ctx.message.text.startsWith('http')) {
+      const urls = ctx.message.text;
+      await ctx.reply('Thank you! The file will be processed now.🦩🦜🦚');
+  console.log(urls);
+
+    // Create a FormData object and append the photo to it
+    const data = new FormData();
+    data.append('urls', urls);
+
+    // Set up options for the API request
+    const options = {
+      method: 'POST',
+      url: API_ENDPOINT,
+      headers: {
+        'X-RapidAPI-Key': 'ff8953e2b2mshfbd85d43df50f19p1b0e1bjsn3a980e29ae31',
+        'X-RapidAPI-Host': 'ocr-nanonets.p.rapidapi.com',
+        ...data.getHeaders(),
+      },
+      data: data
+    };
+
+    try {
+      // Make the API request and log the response
+      const response = await axios.request(options);
+      const rawText = response.data.results[0].page_data[0].raw_text;
+      await ctx.reply(rawText);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+      ctx.reply('Something went wrong!')
+    }  
+    } else {
+      await ctx.reply('Please send me a valid URL or a photo! 🐼🐻🐻‍❄️');
+    }
+    
   }
 });
 
 
 
-
-// Start the bot
+keepAlive();
+// start the bot
 bot.startPolling();
+
